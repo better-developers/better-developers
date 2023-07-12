3d0fdd4131cdf82bf207615a6782f5dabcf6e440
Mikkel O. Laursen <m@ikkel.dk>
2023-07-12 15:53:26 +0200

    Remove unused grid

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -6,13 +6,5 @@ export const Section: FC<BoxProps> = ({ children, ...props }) => (
         <Grid gridTemplateColumns="0.5fr repeat(4, 1fr) 0.5fr" width="100%" minW={0}>
             {children}
         </Grid>
-        <Grid gridTemplateColumns="0.5fr repeat(4, 1fr) 0.5fr" width="100%" height="100%" top="0" position="absolute" zIndex={-2}>
-            <Box width="1px" background="blackAlpha.50" />
-            <Box width="1px" background="blackAlpha.50" />
-            <Box width="1px" background="blackAlpha.50" />
-            <Box width="1px" background="blackAlpha.50" />
-            <Box width="1px" background="blackAlpha.50" />
-            <Box width="1px" background="blackAlpha.50" />
-        </Grid>
     </Box>
 );
