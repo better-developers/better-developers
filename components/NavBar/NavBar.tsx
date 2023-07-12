bcf8fc68d5ace18d2ffe790c40980674e7735972
Mikkel O. Laursen <m@ikkel.dk>
2023-07-12 16:52:59 +0200

    Only add event listener on mount

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@
 import { Flex, Heading, Hide, IconButton, Link, Menu, MenuButton, MenuItem, MenuList, Show, Stack } from '@chakra-ui/react';
 import { useScroll } from 'framer-motion';
 import { useRouter } from 'next/router';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { NavBarContext } from '../../contexts/NavBarContext';
 import { BetterDevelopersLogo } from '../BetterDevelopersLogo/BetterDevelopersLogo';
 import { ContactButton } from '../ContactButton/ContactButton';
@@ -16,7 +16,7 @@ export const NavBar = () => {
     const contextColor = color === 'light' ? 'white' : color === 'dark' ? 'primary' : 'primary';
     const fontColor = isTop ? contextColor : 'primary';
 
-    scrollY.on('change', (y) => setIsTop(y <= 0));
+    useEffect(() => scrollY.on('change', (y) => setIsTop(y <= 0)), [scrollY]);
 
     const links = [
         { href: 'services', text: 'Services' },
@@ -38,8 +38,7 @@ export const NavBar = () => {
                 borderBottom={isTop ? 'none' : '1px solid rgba(0,0,0, 0.1)'}
                 boxShadow={isTop ? 'none' : 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;'}
                 transition="background-color 300ms, color 500ms, box-shadow 300ms, border-bottom 500ms 500ms"
-                zIndex="100"
-            >
+                zIndex="100">
                 <Stack direction="row" spacing="24px" alignItems="center" cursor="pointer" onClick={() => router.push('/')}>
                     <BetterDevelopersLogo />
 
