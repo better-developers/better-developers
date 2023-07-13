8fe05862408a11462fc566445a41c61a400968ec
Mikkel O. Laursen <m@ikkel.dk>
2023-07-13 13:07:05 +0200

    [skip-ci]: WIP
    [skip-ci] WIP

--- components/SectionItem/SectionItem.tsx
+++ components/SectionItem/SectionItem.tsx
@@ -2,7 +2,7 @@ import { GridItem, GridItemProps } from '@chakra-ui/react';
 import { FC, PropsWithChildren } from 'react';
 
 export const SectionItem: FC<PropsWithChildren<GridItemProps>> = ({ children, ...props }) => (
-    <GridItem {...props} minW={0} px={props.px ?? 4}>
+    <GridItem {...props} colStart={[1, 1, 1, (props.colStart as any) ?? 'auto']} minW={0} px={props.px ?? 4}>
         {children}
     </GridItem>
 );
