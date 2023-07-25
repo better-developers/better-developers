ed68f64ac2fb06dcccf26fa6717b16fd5c74adc1
Mikkel O. Laursen <m@ikkel.dk>
2023-07-25 18:15:12 +0200

    Test again again

--- components/DynamicThemeColor/DynamicThemeColor.tsx
+++ components/DynamicThemeColor/DynamicThemeColor.tsx
@@ -1,18 +1,27 @@
+import { useToken } from '@chakra-ui/react';
 import Head from 'next/head';
 import { useIsTop } from '../../hooks/useIsTop';
 import { HexColor } from '../../types/hex-color';
+import { ThemeColor } from '../../types/theme-color';
 
 type DynamicThemeColorProps = {
-    color: HexColor;
+    color: HexColor | ThemeColor;
 };
 
 export const DynamicThemeColor: React.FC<DynamicThemeColorProps> = ({ color }) => {
+    const [themeColor] = useToken('colors', color);
     const isTop = useIsTop();
-    const themeColor = isTop ? color : '#FFF';
+
+    const isHexColor = color.startsWith('#');
+
+    // if provided color is a hex color, use it - otherwise get computed theme color
+    const computedColor = isHexColor ? color : themeColor;
+
+    const resultColor = isTop ? computedColor : '#FFF';
 
     return (
         <Head>
-            <meta name="theme-color" content={themeColor}></meta>
+            <meta name="theme-color" content={resultColor}></meta>
         </Head>
     );
 };
