3ce5c3e011553e8a86338b3b37d908c9e17402ba
Mikkel O. Laursen <m@ikkel.dk>
2023-07-25 13:21:46 +0200

    Fix video

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -19,10 +19,18 @@ const Services: React.FC = () => {
                 <HeroSection
                     colors={{ base: 'brand.50', primary: '#3E1905', secondary: '#1C1E29', tertiary: '#3B3653' }}
                     backgroundElement={
-                        <video src="/video/casette.mp4" autoPlay muted loop width="100%" poster="/video/casette-placeholder.jpg" />
+                        <video
+                            src="/video/casette.mp4"
+                            autoPlay
+                            muted
+                            loop
+                            width="100%"
+                            poster="/video/casette-placeholder.jpg"
+                            style={{ objectFit: 'cover', height: '100%' }}
+                        />
                     }>
                     <SectionItem colStart={[1, 1, 2]} colSpan={[5, 5, 4]} height={750}>
-                        <Center flexDirection="column" gap={4} h="60%">
+                        <Center flexDirection="column" gap={4} h={['40%', '40%', '60%']}>
                             <Heading color="#F7A75A" size="sm" opacity={0.5} fontWeight={300}>
                                 Sandheden er, at telefonopkald ofte er mere meningsfyldte end tekstbeskeder.
                             </Heading>
