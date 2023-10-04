e6cd1ba8355d083b7c0011ceb0135917a90e61eb
Mikkel O. Laursen <m@ikkel.dk>
2023-10-04 10:24:38 +0200

    Fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,17 @@
 import { Builder, BuilderComponent } from '@builder.io/react';
+import { Code, Flex, Text } from '@chakra-ui/react';
 
 export default function NotFoundPage() {
     if (Builder.isPreviewing || Builder.isEditing) {
         return <BuilderComponent model="page" />;
     }
 
-    return <h1>404 - Page Not Found</h1>;
+    return (
+        <Flex height="100vh" width="100vw" alignItems="center" justifyContent="center">
+            <Code fontSize="4xl" marginRight="8">
+                404
+            </Code>
+            <Text fontSize="2xl">Page Not Found</Text>
+        </Flex>
+    );
 }
