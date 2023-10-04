359360ad7714980be56ee49dfef88d144db23089
VikLundAnd <49564365+VikLundAnd@users.noreply.github.com>
2023-10-04 13:27:12 +0200

    test

--- pages/api/sendmail/index.ts
+++ pages/api/sendmail/index.ts
@@ -1,8 +1,12 @@
-import { NextApiResponse, NextApiRequest } from 'next'
-
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+type ResponseData = {
+  message: string
+}
+ 
 export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
 ) {
-  return res.status(200).json({})
+  res.status(200).json({ message: 'Hello from Next.js!' })
 }
